5ab56b9c0e3a0d830022644cbe973f406665be5f
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-28 17:14:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+let nome = prompt("qual seu nome?");
+//let cor = prompt("qual sua cor favorita?");
+//console.log("A cor favorita de " + nome + "é "+ cor);
+//console.log(`a cor favorita de ${nome} é ${cor}`);
+//retornar o numero de caractéres em uma string;
+//console.log(nome.length) ;
+// retornar a string convertida para minusculo;
+//console.log(nome.toUppercase());
+// retira os epaços ao da string
+//console.log(nome.trim());
+// método para buscar determinado texto dentro de uma string;
+//console.log(nome.includes("cenoura"));
+// troca o texto da string pelo texto fornecido
+// replaceAll("texto antigo" , " texto novo")
+//console.log(nome.replaceAll ("cenoura", "batata"));
+
+//let frase = prompt ("digite uma frase");
+//console.log(frase.toUpperCase());
+//console.log(frase.replace ("o","i" ));
+//console.log(frase.length);
+
+//arrays
+//let listaDeexercicios = ["elevacao lateral", "supino", "voador"];
+//console.log(listaDeExercicios (0));
+//console.log(listaDeExercicios[1]);
+//console.log(listaDeExercios[2]);
+//console.log(listaDeExercicios);
+
+//let cachorros = ["golden", "pitbull", "pug" , "pastor alemao" , "terra nova"];
+//let numero = Number(prompt("digite um numero 0 a 4"));
+//console.log(cachorros[numero]);
+
+//const pokemon = ["bulbasauro" , "squirtle" , " charmander"];
+//console.log(pokemon.length);
+//console.log(pokemon[0].length);
+
+//const seriesBoas = [Breaking bad" , "Brooklyn Nine-nine"]
+//console.log(seriesboas.includes Breaking Bad));
+//console.log(series boas.includes (Game of thrones)); // false
+
+let imstrumentos = ["violao" , "guitarra" , "bateria"];
+console.log(instrumentos);
+instrumentos.push("teclado");
+console.log(instrumentos);
+instrumentos.pop();
+console.log(instrumentos);
+instrumentos.pop();
+instrumentos.splice(2,2);
+console.log(instrumentos);
+
+let numeros = ["1" , "2" , "3", "4", "5", "6" ];
+console.log(numeros.length);
+numeros.push(7)
+console.log(numeros);
+console.splice(3,2);
+console.log(numeros.length);
